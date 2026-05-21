833aa548ad5f2eaf2f2dcb1a9da0874597e8d7d7
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-05-21 12:57:41 +0900

    test

--- foo/main.tf
+++ foo/main.tf
@@ -1,18 +1,17 @@
-# test
 resource "null_resource" "foo" {}
 
 module "db" {
   source = "../modules/db"
 }
 
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "3.3.0"
-    }
-  }
-}
-
-provider "null" {}
+# terraform {
+#   required_version = ">= 1.0.0"
+#   required_providers {
+#     null = {
+#       source  = "hashicorp/null"
+#       version = "3.3.0"
+#     }
+#   }
+# }
+#
+# provider "null" {}
