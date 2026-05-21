8cbf16255539a629898e284976e40cc19686889c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-05-21 22:09:53 +0900

    test

--- foo/main.tf
+++ foo/main.tf
@@ -11,7 +11,7 @@ resource "github_branch_protection" "good_example" {
 }
 
 terraform {
-  required_version = ">= 1.0.0"
+  # required_version = ">= 1.0.0"
   required_providers {
     null = {
       source  = "hashicorp/null"
