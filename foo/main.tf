19e25d92968c143677a39b610d6ee8a46d5baffa
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-05-21 21:08:25 +0900

    test trivy

--- foo/main.tf
+++ foo/main.tf
@@ -4,14 +4,26 @@ module "db" {
   source = "../modules/db"
 }
 
-# terraform {
-#   required_version = ">= 1.0.0"
-#   required_providers {
-#     null = {
-#       source  = "hashicorp/null"
-#       version = "3.3.0"
-#     }
-#   }
-# }
-#
-# provider "null" {}
+resource "github_branch_protection" "good_example" {
+  repository_id = "example"
+  pattern       = "main"
+  # require_signed_commits = true
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.3.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "null" {}
+
+provider "github" {}
