d3889cb743cb26fefa0270329ca9d06e2d16de34
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2026-05-21 09:44:40 +0900

    fix tflint (#31)
    * fix tflint
    
    * chore: update .terraform.lock.hcl
    
    ---------
    
    Co-authored-by: szksh-lab-tfaction-test[bot] <260139119+szksh-lab-tfaction-test[bot]@users.noreply.github.com>

--- foo/main.tf
+++ foo/main.tf
@@ -3,3 +3,15 @@ resource "null_resource" "foo" {}
 module "db" {
   source = "../modules/db"
 }
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.3.0"
+    }
+  }
+}
+
+provider "null" {}
