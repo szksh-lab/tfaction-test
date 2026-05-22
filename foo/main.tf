0be08cb0df3a6c4fc42bee74da594bd4b23b505d
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2026-05-22 17:09:30 +0900

    lint (#35)
    * lint
    
    * test
    
    * chore: update .terraform.lock.hcl
    
    * test
    
    * test trivy
    
    * chore: update .terraform.lock.hcl
    
    * allow github provider
    
    * update
    
    * test
    
    * test
    
    * update
    
    * update
    
    * chore: update .terraform.lock.hcl
    
    * update
    
    ---------
    
    Co-authored-by: szksh-lab-tfaction-test[bot] <260139119+szksh-lab-tfaction-test[bot]@users.noreply.github.com>

--- foo/main.tf
+++ foo/main.tf
@@ -1,9 +1,14 @@
-# test
 resource "null_resource" "foo" {}
 
 module "db" {
   source = "../modules/db"
 }
+
+# resource "github_branch_protection" "good_example" {
+#   repository_id          = "example"
+#   pattern                = "main"
+#   require_signed_commits = true
+# }
 
 terraform {
   required_version = ">= 1.0.0"
@@ -12,7 +17,13 @@ terraform {
       source  = "hashicorp/null"
       version = "3.3.0"
     }
+    # github = {
+    #   source  = "integrations/github"
+    #   version = "~> 6.0"
+    # }
   }
 }
 
 provider "null" {}
+
+# provider "github" {}
