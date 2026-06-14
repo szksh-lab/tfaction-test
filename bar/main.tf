5dc6140e1fd888eca43e619d52fdac1e1126f1e8
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2026-06-14 12:32:29 +0900

    configure null provider

--- bar/main.tf
+++ bar/main.tf
@@ -1,1 +1,12 @@
 resource "null_resource" "foo" {}
+
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.3.0"
+    }
+  }
+}
+
+provider "null" {}
